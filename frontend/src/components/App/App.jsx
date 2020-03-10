001249b47cd03bca65c8a1c0724168a50551223f
Pavel Klochkov <paul.klochkov@gmail.com>
2020-03-10 23:40:35 +0100

    Add Players list

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import * as styles from "./App.module.scss";
 import { Player, PlayerForm, PlayerList } from "../Player";
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [currentPlayer, setCurrentPlayer] = useState({});
+  const playersUrl = "http://localhost:3100/players";
+
+  useEffect(() => {
+    const getPlayers = async url => {
+      const playersData = await axios.get(url);
+
+      setPlayers(playersData.data);
+    };
+
+    getPlayers(playersUrl);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">App header</header>
+    <div className="container">
+      <div className="row">
+        <div className="col s12">Menu</div>
+        <div className="row">
+          <div className="col s3">
+            {players[0] && <PlayerList players={players}/>}
+          </div>
+          <div className="col s9">
+            <Player />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <PlayerForm />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/Player/PlayerList.jsx
+++ frontend/src/components/Player/PlayerList.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const PlayerList = () => {
+const PlayerList = props => {
+  console.log("players", props);
+
   return (
     <div>
-      Player List
+      <ul className="collection with-header">
+        <li className="collapsible-header">
+          <h4>Players</h4>
+        </li>
+        {props.players.map(player => (
+          <li className="collection-item" key={player._id}>
+            <div>
+              <a href="#!">
+                {player.firstName} {player.lastName}
+              </a>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
