7d3cec0ed8225af33cb1d7330f1f29dedff80203
Pavel Klochkov <paul.klochkov@gmail.com>
2020-03-10 22:57:11 +0100

    Add scss-modules support

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
+import * as styles from "./App.module.scss";
+import { Player, PlayerForm, PlayerList } from "../Player";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        App header
-      </header>
+      <header className="App-header">App header</header>
     </div>
   );
 }
